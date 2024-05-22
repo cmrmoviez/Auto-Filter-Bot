90fd3b63e0288768050fc4b67568ec8a90579128
cmr <cmrmoviez@gmail.com>
2024-05-22 12:20:16 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -36,7 +36,7 @@ async def is_subscribed(bot, query, channel):
     if AUTH_CHANNEL:
         channel.append(AUTH_CHANNEL)
     for id in channel:
-        chat = await bot.get_chat(int(id))
+        chat = await bot.get_chat(str(id))
         try:
             await bot.get_chat_member(id, query.from_user.id)
         except UserNotParticipant:
